8791992272b03eeb955addd9c8fc99feba57f4d3
Hanifp <hanifwanda0@gmail.com>
2022-10-19 14:07:20 +0700

    installasi

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\heloController;
+use App\http\Controllers\SiswaController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +17,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    // return $request->user();
+// });
+
+/*
+Task
+URL : http://localhost/api/halo
+METHOD : GET
+Exec: function
+Return: JSON => ("me": "hanif")
+*/
+
+Route::get('halo', function(){
+    return["me" => "hanif"];
 });
+
+Route::resource('heloController', heloController::class);
+
+Route::resource('siswa', SiswaController::class);
